84f65bafe172b968f7abd3f140f41212a581b953
Alejandro Reguilón Escalona <elregui.1996@gmail.com>
2018-04-22 19:33:52 +0200

    version3

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import calculadora
+from django.http import HttpResponse
 
 # Create your views here.
+def suma(request, op1, op2):
+    return HttpResponse(calculadora.suma(op1, op2))
+def resta(request, op1, op2):
+    return HttpResponse(calculadora.resta(op1, op2))
+def mul(request, op1, op2):
+    return HttpResponse(calculadora.multiplicacion(op1, op2))
+def div(request, op1, op2):
+    return HttpResponse(calculadora.division(op1, op2))
+def barra(request, op1, op2):
+    return HttpResponse("introduce operandos")
+    
